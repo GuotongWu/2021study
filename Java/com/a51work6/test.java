f57782a10c91d6cd3704ccc86d29cf64be6ef402
GuotongWu <wuguotongswjtu@gmail.com>
2021-02-18 10:19:24 +0800

    new Java

---
+++ Java/com/a51work6/test.java
@@ -0,0 +1,15 @@
+package com.a51work6;
+
+public class test{
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
+
+class testA{
+
+}
+
+class testB{
+
+}
