044b9f32f24ab51865f33339d9ce652a18d385fb
GuotongWu <wuguotongswjtu@gmail.com>
2021-03-19 21:40:25 +0800

    MATLAB lab2 finished

--- MATLAB/Simulation_Modeling/lab1/step.m
+++ MATLAB/Simulation_Modeling/lab1/step.m
@@ -1,12 +1,13 @@
-% y = 4*exp(-4*t)*cos(10*t) + 10*sin(10*t)  (t>=0)
+% lab1.m 计算单位阶跃响应
+% y = -1*exp(-4*t).*cos(10*t) + 1; (t>=0)
 clc,clear,close all;
 t = 0:0.001:2;
-y = 4*exp(-4*t).*cos(10*t) + 10*sin(10*t);
+y = -1*exp(-4*t).*cos(10*t) + 1;
 figure()
-plot(t,y)
-xlabel('time')
+plot(t, y, 'r', 'LineWidth',2)
+xlabel('t/s')
 ylabel('y(t)')
 grid on
 title('单位阶跃响应')
 % 主程序调用maxV()
-[max_y, max_t] = maxV(y,t)
+[max_y, max_t] = maxV_copy_1(y,t)
