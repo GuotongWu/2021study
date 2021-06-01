b00f4f3865a202e3975ef9c4f8ba533a110c42f0
GuotongWu <wuguotongswjtu@gmail.com>
2021-06-01 12:46:16 +0800

    pdf

--- Leetcode/test1.cpp
+++ Leetcode/test1.cpp
@@ -1,9 +1,43 @@
 #include<iostream>
 using namespace std;
 
+const int N = 1e5+10;
+int p[N], s[N];
+int n,m;
+
+int find(int x){
+    if(p[x] != x)
+        p[x] = find(p[x]);
+    return p[x];
+}
+
 int main(){
-    cout<<(5^6)<<endl;
-    cout<<(5^7)<<endl;
-    cout<<(6^7)<<endl;
+    scanf("%d%d", &n, &m);
+    for(int i=0; i<n; ++i){
+        p[i] = i;
+        s[i] = 1;
+    }
+        
+    while(m--){
+        string op;
+        int x, y;
+        cin>>op;
+        if(op == "C"){
+            cin>>x>>y;
+            p[find(x)] = find(y);
+            s[find(y)] += s[find(x)];
+        }
+        else if(op == "Q1"){
+            cin>>x>>y;
+            if(find(x) == find(y))
+                printf("Yes\n");
+            else
+                printf("No\n");
+        }
+        else{
+            cin>>x;
+            printf("%d\n", s[find(x)]);
+        }
+    }
     return 0;
 }
