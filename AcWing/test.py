0356b0bc90372c435ea661691b6ed149225a2a04
GuotongWu <wuguotongswjtu@gmail.com>
2021-12-13 08:51:13 +0800

    acwing note

--- AcWing/test.py
+++ AcWing/test.py
@@ -1,4 +1,14 @@
 import numpy as np
-import torch
+def deculated_reward(rewards, lambd):
+    g = rewards[-1]
+    ans = [g]
+    for i in range(len(rewards)-1):
+	    g = g*lambd + rewards[-i-2]
+	    ans.append(g)
+    ans.reverse()
+    return np.array(ans)
 
-print()
+rewards = np.array([1,2,3])
+lambd = 0.8
+
+print(type(deculated_reward(rewards, lambd)))
